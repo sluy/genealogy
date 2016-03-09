418d55b3b9938a6184a80c8fc1ea515d57c5dcba
Stefan Luy <sluy1283@gmail.com>
2016-03-09 01:43:23 -0430

    Update and rename LICENSE.md to ejercicio.php

---
+++ ejercicio.php
@@ -0,0 +1,15 @@
+<?php
+//Incluimos requeridos
+include_once("genealogy.php");
+include_once("member.php");
+//Iniciamos Genealogy
+$g = new Genealogy();
+//Creamos todas las relaciones
+$g->get("Norelis Santander")->addParents(["Florelia Santander","Isidro Garcia"]);
+$g->get("Antonio Garcia")->addParents(["Florelia Santander","Isidro Garcia"]);
+$g->get("Jesus Marques")->addParent("Carmen Perez");
+$g->get("Florelia Santander")->addParent("Carmen Perez");
+$g->get("Isidro Garcia")->addParents(["Yolanda Zambrano", "Jesus Garcia"]);
+$g->get("Jesus Garcia")->addParent("Maria Herrera");
+//imprimimos el arbol
+$g->get("Maria Herrera")->printTree();
